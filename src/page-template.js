b54dc8194b6f48d8818694204fa397703393d116
Kiran Kanda <kkanda@dons.usfca.edu>
2021-05-15 21:46:40 -0700

    generated html file on its own from command line

--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,5 @@
-
+const generatePage = (name, github) => {
+    return `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -9,8 +10,11 @@
     </head>
 
   <body>
-    <h1>jack</h1>
-    <h2><a href="https://github.com/https://github.com/winterkanda/portfolio-generator">Github</a></h2>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
   </body>
   </html>
-  
+  `;
+};
+
+module.exports = generatePage;
