e9a2e35b37802f4003a8f0d399f9a1b429a9697d
Kiran Kanda <kkanda@dons.usfca.edu>
2021-05-15 21:54:43 -0700

    refractored code again

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
-
 const generatePage = require('./src/page-template.js');
 
 const profileDataArgs = process.argv.slice(2);
 
+console.log(profileDataArgs);
+
 const [name, github] = profileDataArgs;
 
+console.log(name, github);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw err;
   
     console.log('Portfolio complete! Check out index.html to see the output!');
 });
