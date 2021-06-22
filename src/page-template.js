b99a45b4836f2abb0bf343d0beeed06fc3418c8e
Kiran Kanda <kkanda@dons.usfca.edu>
2021-06-21 19:20:38 -0700

    cleaned up code

--- src/page-template.js
+++ src/page-template.js
@@ -1,18 +1,17 @@
-// create the about section
+//create the about section
 const generateAbout = aboutText => {
   if (!aboutText) {
     return '';
   }
 
   return `
-    <section class="my-3" id="about">
-      <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
-      <p>${aboutText}</p>
-    </section>
+  <section class="my-3" id="about">
+    <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+    <p>${aboutText}</p>
+  </section>
   `;
 };
 
-// create the projects section
 const generateProjects = projectsArr => {
   return `
     <section class="my-3" id="portfolio">
@@ -26,7 +25,7 @@ const generateProjects = projectsArr => {
             <h3 class="portfolio-item-title text-light">${name}</h3>
             <h5 class="portfolio-languages">
               Built With:
-              ${languages.join(',')}
+              ${languages.join(', ')}
             </h5>
             <p>${description}</p>
             <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
@@ -34,7 +33,6 @@ const generateProjects = projectsArr => {
         `;
         })
         .join('')}
-
       ${projectsArr
         .filter(({ feature }) => !feature)
         .map(({ name, description, languages, link }) => {
@@ -56,43 +54,41 @@ const generateProjects = projectsArr => {
   `;
 };
 
-
-// export function to generate entire page
 module.exports = templateData => {
-  // destructure page data by section
+  console.log(templateData);
+
+  // destructure projects and about data from templateData based on their property;
   const { projects, about, ...header } = templateData;
 
-  return `
-  <!DOCTYPE html>
-  <html lang="en">
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="style.css">
+    </head>
   
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./style.css">
-  </head>
-  
-  <body>
-    <header>
-      <div class="container flex-row justify-space-between align-center py-3">
-        <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
-        <nav class="flex-row">
-          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">GitHub</a>
+    <body>
+    <div class="container flex-row justify-space-between align-center py-3">
+      <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+      <nav class="flex-row">
+        <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">Github</a>
         </nav>
-      </div>
-    </header>
-    <main class="container my-5">
-      ${generateAbout(about)}
-      ${generateProjects(projects)}
-    </main>
-    <footer class="container text-center py-3">
-      <h3 class="text-dark">&copy;2020 by ${header.name}</h3>
-    </footer>
-  </body>
-  </html>
-  `;
-}
+        </div>
+      </header>
+      <main class="container my-5">
+        ${generateAbout(about)}
+        ${generateProjects(projects)}
+      </main>
+      <footer class="container text-center py-3">
+        <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+      </footer>
+    </body>
+    </html>
+    `;
+};
